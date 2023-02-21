4298f9592844be16c92e313a2163f720067bd6f5
AlvaroMoraS <125828485+AlvaroMoraS@users.noreply.github.com>
2023-02-20 21:06:45 -0300

    update2

---
+++ function.js
@@ -0,0 +1,10 @@
+
+function validar(){
+    let password = document.getElementById("password").value;
+    /*if (password == 123456){
+        alert("...Bienvenido...\n muajuajua \n te lo advertí....");
+    }else{
+        alert("...Password incorrecto");
+    }*/
+    (password == 123456) ? alert("...Bienvenido...\n muajuajua \n te lo advertí....") : alert("...Password incorrecto");
+}
